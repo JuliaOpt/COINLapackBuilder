0ebdf84fe01fbdc09bd9c02f454a04928509ae86
juan-pablo-vielma <jvielma@mit.edu>
2018-08-22 20:25:48 -0400

    Add autogenerated files to build COINLapackBuilder

---
+++ README.md
@@ -0,0 +1,9 @@
+# COINLapackBuilder
+
+[![Build Status](https://travis-ci.org/juan-pablo-vielma/COINLapackBuilder.svg?branch=master)](https://travis-ci.org/juan-pablo-vielma/COINLapackBuilder)
+
+This repository builds binary artifacts for the COINLapackBuilder project. Binary artifacts are automatically uploaded to
+[this repository's GitHub releases page](https://github.com/juan-pablo-vielma/COINLapackBuilder/releases) whenever a tag is created
+on this repository.
+
+This repository was created using [BinaryBuilder.jl](https://github.com/JuliaPackaging/BinaryBuilder.jl)
---
+++ build_tarballs.jl
@@ -0,0 +1,58 @@
+# Note that this script can accept some limited command-line arguments, run
+# `julia build_tarballs.jl --help` to see a usage message.
+using BinaryBuilder
+
+name = "COINLapackBuilder"
+version = v"1.5.6"
+
+# Collection of sources required to build COINLapackBuilder
+sources = [
+    "https://github.com/coin-or-tools/ThirdParty-Lapack/archive/releases/1.5.6.tar.gz" =>
+    "c625dbb227e54e496430ffa708ddf23df5dbf173a0fcf570e1c249e13e411ba1",
+
+]
+
+# Bash recipe for building across all platforms
+script = raw"""
+cd $WORKSPACE/srcdir
+cd ThirdParty-Lapack-releases-1.5.6/
+./get.Lapack 
+update_configure_scripts
+mkdir build
+cd build/
+../configure --prefix=$prefix --with-pic --disable-pkg-config --with-blas="$prefix/lib/libcoinblas.a -lgfortran" --host=${target} --enable-shared --enable-static --enable-dependency-linking lt_cv_deplibs_check_method=pass_all
+make -j${nproc}
+make install
+
+"""
+
+# These are the platforms we will build for by default, unless further
+# platforms are passed in on the command line
+platforms = [
+    Linux(:i686, :glibc),
+    Linux(:x86_64, :glibc),
+    Linux(:aarch64, :glibc),
+    Linux(:armv7l, :glibc, :eabihf),
+    Linux(:powerpc64le, :glibc),
+    Linux(:i686, :musl),
+    Linux(:x86_64, :musl),
+    Linux(:aarch64, :musl),
+    Linux(:armv7l, :musl, :eabihf),
+    MacOS(:x86_64),
+    Windows(:i686),
+    Windows(:x86_64)
+]
+
+# The products that we will ensure are always built
+products(prefix) = [
+    LibraryProduct(prefix, "libcoinlapack", :libcoinlapack)
+]
+
+# Dependencies that must be installed before this package can be built
+dependencies = [
+    "https://github.com/juan-pablo-vielma/COINBLASBuilder/releases/download/v1.4.6-beta2/build_COINBLASBuilder.v1.4.6.jl"
+]
+
+# Build the tarballs, and possibly a `build.jl` as well.
+build_tarballs(ARGS, name, version, sources, script, platforms, products, dependencies)
+
