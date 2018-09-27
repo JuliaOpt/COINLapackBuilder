c904c776a3e619b703026a81d2281be9d65a751c
Juan Pablo Vielma <jvielma@mit.edu>
2018-09-27 11:18:06 -0400

    Update build_tarballs.jl

--- build_tarballs.jl
+++ build_tarballs.jl
@@ -57,7 +57,7 @@ products(prefix) = [
 
 # Dependencies that must be installed before this package can be built
 dependencies = [
-    "https://github.com/juan-pablo-vielma/COINBLASBuilder/releases/download/v1.4.6/build_COINBLASBuilder.v1.4.6.jl"
+    "https://github.com/JuliaOpt/COINBLASBuilder/releases/download/v1.4.6-1/build_COINBLASBuilder.v1.4.6.jl"
 ]
 
 # Build the tarballs, and possibly a `build.jl` as well.
